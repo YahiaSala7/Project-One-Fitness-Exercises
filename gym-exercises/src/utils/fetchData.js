39369fa413218c009f7c12673971abcebf8233bd
Yahia <yahia00000001@gmail.com>
2023-06-20 15:38:32 +0300

    deleting file and some code

--- gym-exercises/src/utils/fetchData.js
+++ gym-exercises/src/utils/fetchData.js
@@ -2,7 +2,7 @@ import axios from "axios";
 export const exerciseOptions = {
   method: "GET",
   headers: {
-    "X-RapidAPI-Key": "ac0f01db2fmsh1c8b0dc31449dd6p139630jsn5aaa2e064364",
+    "X-RapidAPI-Key": ,
     "X-RapidAPI-Host": "exercisedb.p.rapidapi.com",
   },
 };
@@ -10,7 +10,7 @@ export const exerciseOptions = {
 export const youtubeOptions = {
   method: "GET",
   headers: {
-    "X-RapidAPI-Key": "ac0f01db2fmsh1c8b0dc31449dd6p139630jsn5aaa2e064364",
+    "X-RapidAPI-Key": ,
     "X-RapidAPI-Host": "youtube-search-and-download.p.rapidapi.com",
   },
 };
